659c80f3440b15a196763a24d03fda2c277f1164
Steven Zamorano <stevenzamorano.code@gmail.com>
2024-08-19 14:48:15 -0400

    Adding your favorite places functionality

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +13,35 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+
+export class UserPlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() 
+  {
+    this.isFetching.set(true);
+    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/user-places').pipe(map((resData) => resData.places)).subscribe({
+      next: (places) => 
+        {
+          this.places.set(places)
+        }, 
+      error: (error) =>
+      {
+        console.log(error);
+        this.error.set('Something went wrong fetching your favorite places. Please try again later.');
+      },
+      complete: () => 
+        {
+          this.isFetching.set(false);
+        }
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
 }
