fb2d378a70f10c98ede4733aef44e2947ef1fe9d
Steven Zamorano <stevenzamorano.code@gmail.com>
2024-08-19 16:36:35 -0400

    Manage http-loaded data via the service

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -16,20 +16,17 @@ import { PlacesService } from '../places.service';
 })
 
 export class UserPlacesComponent implements OnInit {
-  places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
   error = signal('');
   private placesService = inject(PlacesService);
   private destroyRef = inject(DestroyRef);
+  places = this.placesService.loadedUserPlaces;
+
 
   ngOnInit() 
   {
     this.isFetching.set(true);
     const subscription = this.placesService.loadUserPlaces().subscribe({
-      next: (places) => 
-        {
-          this.places.set(places)
-        }, 
       error: (error) =>
       {
         console.log(error);
